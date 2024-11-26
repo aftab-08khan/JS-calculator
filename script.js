62796fbe06b4e6f095a0f105b03ec618c52656b6
aftab-08khan <akkhanaftab08@gmail.com>
2024-11-26 17:54:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+const input = document.querySelector("input");
+const btn = document.querySelectorAll("button");
+let string = "";
+const arr = Array.from(btn);
+arr.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
+
+// let string = "";
+// const arr = Array.from(btn);
+// arr.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     if (e.target.innerHTML == "=") {
+//       string = eval(string);
+//       input.value = string;
+//     } else if (e.target.innerHTML == "AC") {
+//       string = "";
+//       input.value = string;
+//     } else if (e.target.innerHTML == "DEL") {
+//       string = string.substring(0, string.length - 1);
+//       input.value = string;
+//     } else {
+//       string += e.target.innerHTML;
+//       input.value = string;
+//     }
+//   });
+// });
